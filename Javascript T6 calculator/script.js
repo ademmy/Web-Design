e4eca2bcc8d8b46c35d5c1749a6601ebbc614761
Arbab Arain <arbabanjum0347@gmail.com>
2020-05-29 20:53:09 +0500

    JS T6 calculator

---
+++ Javascript T6 calculator/script.js
@@ -0,0 +1,30 @@
+
+function add()
+{
+    a = parseInt(document.getElementById('number1').value);
+    b = parseInt(document.getElementById('number2').value);
+    c = a + b;
+    window.alert("Addition of Two Numbers = "+ c);
+
+}; 
+function mul()
+{
+    a = parseInt(document.getElementById('number1').value);
+    b = parseInt(document.getElementById('number2').value);
+    c = a * b;
+    window.alert("Addition of Two Numbers = "+ c);
+}; 
+function sub()
+{
+    a = parseInt(document.getElementById('number1').value);
+    b = parseInt(document.getElementById('number2').value);
+    c = a - b;
+    window.alert("Addition of Two Numbers = "+ c);
+}; 
+function divide()
+{
+    a = parseInt(document.getElementById('number1').value);
+    b = parseInt(document.getElementById('number2').value);
+    c = a / b;
+    window.alert("Addition of Two Numbers = "+ c);
+}; 
